834b6bfe018d7e58e12e597f55cb733fee26194a
mvorotniak <v-mvorotniak@vrbo.com>
2021-11-11 03:09:18 -0800

    Initial commit

---
+++ src/test/java/com/mvorodeveloper/spring5webapplication/Spring5WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvorodeveloper.spring5webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
