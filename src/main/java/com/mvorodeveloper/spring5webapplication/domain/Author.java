cf8d9cb85e7d9ff91fd5d64e6f665458fab9a89d
Mariana Developer <87234445+mvorotniak@users.noreply.github.com>
2021-11-11 04:23:17 -0800

    Adding jpa entities (#1)
    * Created POJOs
    
    * Added JPA ID's
    
    * Adding relationship
    
    Co-authored-by: mvorotniak <v-mvorotniak@vrbo.com>

---
+++ src/main/java/com/mvorodeveloper/spring5webapplication/domain/Author.java
@@ -0,0 +1,64 @@
+package com.mvorodeveloper.spring5webapplication.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String name, String surname, Set<Book> books) {
+        this.name = name;
+        this.surname = surname;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
---
+++ src/main/java/com/mvorodeveloper/spring5webapplication/domain/Book.java
@@ -0,0 +1,72 @@
+package com.mvorodeveloper.spring5webapplication.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private int numOfPages;
+
+    /**
+     * The "author_book" table is going to hold the many-to-many relationship between Books and Authors
+     * by linking the "book_id" with an "author_id"
+     */
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String name, int numOfPages, Set<Author> authors) {
+        this.name = name;
+        this.numOfPages = numOfPages;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
