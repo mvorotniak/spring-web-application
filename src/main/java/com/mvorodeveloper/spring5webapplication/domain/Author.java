be22d39c52a17cee049c063203136f83ca2af1c8
mvorotniak <v-mvorotniak@vrbo.com>
2021-11-11 04:07:01 -0800

    Added JPA ID's

--- src/main/java/com/mvorodeveloper/spring5webapplication/domain/Author.java
+++ src/main/java/com/mvorodeveloper/spring5webapplication/domain/Author.java
@@ -2,7 +2,17 @@ package com.mvorodeveloper.spring5webapplication.domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
     private String surname;
@@ -15,6 +25,14 @@ public class Author {
         this.name = name;
         this.surname = surname;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
--- src/main/java/com/mvorodeveloper/spring5webapplication/domain/Book.java
+++ src/main/java/com/mvorodeveloper/spring5webapplication/domain/Book.java
@@ -2,7 +2,17 @@ package com.mvorodeveloper.spring5webapplication.domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
     private int numOfPages;
@@ -15,6 +25,14 @@ public class Book {
         this.name = name;
         this.numOfPages = numOfPages;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
