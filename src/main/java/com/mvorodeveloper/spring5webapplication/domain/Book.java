269b42e0656ce627ffd4af489fcc14f31a01b3b3
mvorotniak <v-mvorotniak@vrbo.com>
2021-11-11 04:43:49 -0800

    Implemented toString()

--- src/main/java/com/mvorodeveloper/spring5webapplication/domain/Book.java
+++ src/main/java/com/mvorodeveloper/spring5webapplication/domain/Book.java
@@ -72,6 +72,16 @@ public class Book {
     }
 
     @Override
+    public String toString() {
+        return "Book{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", numOfPages=" + numOfPages +
+            ", authors=" + authors +
+            '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
